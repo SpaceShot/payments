b8c00dde5e2dd9ab108c4be43534cdc534f9056c
Chris Gomez <cagomez@cellbycell.com>
2015-02-04 21:02:59 -0500

    models and web files that were left untracked

---
+++ Payments.Api/Models/Homeowner.cs
@@ -0,0 +1,11 @@
+namespace Payments.Api.Models
+{
+    public class Homeowner
+    {
+        public string Name;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zip;
+    }
+}

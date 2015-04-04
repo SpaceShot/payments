fe5fb14365fc9881c381bdfc8f1f69118c59ba99
Mushiwulf <mushiwulf@gmail.com>
2015-04-04 13:14:21 -0700

    Adds residence.js in services and payments-directive.js in directives

--- Payments/scripts/directives/payments-directive.js
+++ Payments/scripts/directives/payments-directive.js
@@ -1,7 +1,12 @@
 angular.module('Payments', [])
     .directive('paymentsResident', function(){
     return {
-        restrict: E,
-        templateUrl: 'payments-resident.html'
+        restrict: "E",
+        templateUrl: 'payments-resident.html',
+        controller: function($scope){
+            $scope.id = "1001";
+            $scope.owner = "Sally Mae";
+            $scope.balance = "0";
+        }
     };
 });
---
+++ Payments/scripts/services/residence.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Sean on 4/3/2015.
+ */
+angular.module("Payments")
+    .factory("Residence", function ResidenceFactory(){
+        return{}
+    });
